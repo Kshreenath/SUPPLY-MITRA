9e4bfcb598be4abb7136d9f13fa98b12a6002607
K Shreenath Bohra <kshreenathbohra@gmail.com>
2020-05-01 11:53:47 +0530

    Intitial Commit

---
+++ src/appinventor/ai_konenrocks/Food_Notifier/Screen2.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"206","Source":"Form","Properties":{"$Name":"Screen2","$Type":"Form","$Version":"27","ActionBar":"True","AppName":"Food_Notifier","BackgroundImage":"MITAPP.jpg","ScreenOrientation":"sensor","Theme":"AppTheme.Light.DarkActionBar","Title":"Thanks for your Support Sir !","Uuid":"0"}}
+|#
